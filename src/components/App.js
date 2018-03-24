eef7d75bb9b57b577070740e4d554fa7bf34e68f
Michał Kreft <yomajkel@gmail.com>
2018-03-24 15:50:42 +0100

    add server data loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
+import React from 'react';
+import axios from 'axios';
 import Header from './Header';
-import React from "react";
+import ContestPreview from './ContestPreview';
 
 class App extends React.Component {
+
+    state = {
+        pageHeader: 'Header message (o-o)',
+        contests: []
+    };
+
+    componentDidMount() {
+        axios.get('/api/contests')
+            .then(resp => {
+                this.setState({
+                    contests: resp.data.contests
+                });
+            })
+            .catch(console.error);
+    }
+
+    componentWillUnmount() {
+        debugger;
+    }
+
     render() {
         return <div>
-            <Header message="Header message (o-o)"/>
+            <Header message={this.state.pageHeader} />
             <div>
-                ...
+                {this.state.contests.map(contest =>
+                    <ContestPreview key={contest.id} {...contest} />
+                )}
             </div>
         </div>;
     }
